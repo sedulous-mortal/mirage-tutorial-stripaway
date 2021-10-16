85aac490f29c845b10c7cc24bfb4993d4bd97fa3
Alison Stuart <alisonnicolestuart@gmail.com>
2021-10-16 13:08:14 -0400

    banners are rendering as expected when hardcoded to the get endpoint, but there is no obvious way to point to the seeded server, will research

--- src/server.js
+++ src/server.js
@@ -6,22 +6,59 @@ export default function () {
     models: {
         banners: Model,
     },
+    seeds(server) {
+        server.create('banner', {
+          bannerColor: 'blue',
+          bannerLink: 'https://codespark.com',
+          bannerText: 'This is an awesome banner',
+          bannerIcon: 'https://thefoos-com2.s3.amazonaws.com/flagship/icon_desktop.png',
+          startDate: '2021-01-24T19:16:00.000Z',
+          endDate: '2021-01-31T19:16:00.000Z'
+        });
+        server.create('banner', {
+          bannerColor: 'orange',
+          bannerLink: 'https://codespark.com',
+          bannerText: 'This is another awesome banner',
+          bannerIcon: '',
+          startDate: '2021-02-01T20:14:00.000Z',
+          endDate: '2021-02-07T20:14:00.000Z'
+        });
+        server.create('banner', {
+          bannerColor: 'red',
+          bannerLink: 'https://codespark.com',
+          bannerText: 'This is the awesome banner',
+          bannerIcon: '',
+          startDate: '2021-03-01T19:04:00.000Z',
+          endDate: '2021-03-05T19:04:00.000Z'
+        });
+      },
     routes() {
       this.get("/api/banners", () => ({
-        banners: [
-            { id: 1, bannerText: "Walk the dog" },
-            { id: 2, bannerText: "Take out the trash" },
-            { id: 3, bannerText: "Work out" },
-          ],
+        banners: [{
+            bannerColor: 'blue',
+            bannerLink: 'https://codespark.com',
+            bannerText: 'This is an awesome banner',
+            bannerIcon: 'https://thefoos-com2.s3.amazonaws.com/flagship/icon_desktop.png',
+            startDate: '2021-01-24T19:16:00.000Z',
+            endDate: '2021-01-31T19:16:00.000Z'
+            },
+            {
+            bannerColor: 'orange',
+            bannerLink: 'https://codespark.com',
+            bannerText: 'This is another awesome banner',
+            bannerIcon: '',
+            startDate: '2021-02-01T20:14:00.000Z',
+            endDate: '2021-02-07T20:14:00.000Z'
+            },
+            {
+            bannerColor: 'red',
+            bannerLink: 'https://codespark.com',
+            bannerText: 'This is the awesome banner',
+            bannerIcon: '',
+            startDate: '2021-03-01T19:04:00.000Z',
+            endDate: '2021-03-05T19:04:00.000Z'
+            }]
       }))
     },
   })
 }
-// banners: [{
-//     bannerColor: 'red',
-//     bannerLink: 'https://codespark.com',
-//     bannerText: 'This is the awesome banner',
-//     bannerIcon: '',
-//     startDate: '2021-03-01T19:04:00.000Z',
-//     endDate: '2021-03-05T19:04:00.000Z'
-//   }],
