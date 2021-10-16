157484a4a80025cf900f009eb10eeccefc830756
Alison Stuart <alisonnicolestuart@gmail.com>
2021-10-16 12:48:29 -0400

    it renders the list of reminders, about to find and replace all reminders with banners, then begin strip away

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
 // Welcome to the tutorial!
+import { createServer } from "miragejs"
+
+export default function () {
+  createServer({
+    routes() {
+      this.get("/api/reminders", () => ({
+        reminders: [
+            { id: 1, text: "Walk the dog" },
+            { id: 2, text: "Take out the trash" },
+            { id: 3, text: "Work out" },
+          ],
+      }))
+    },
+  })
+}
+// banners: [{
+//     bannerColor: 'red',
+//     bannerLink: 'https://codespark.com',
+//     bannerText: 'This is the awesome banner',
+//     bannerIcon: '',
+//     startDate: '2021-03-01T19:04:00.000Z',
+//     endDate: '2021-03-05T19:04:00.000Z'
+//   }],
